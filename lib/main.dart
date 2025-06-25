052520ec38ee87a84aa0cdf5eeb3e13f072396ae
romario.luz <romario.luz@detran.ce.gov.br>
2025-06-25 15:14:23 -0300

    Quiz Exercise - List Type and Change, Spread

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:teste/questao.dart';
+import 'package:teste/resposta.dart';
+
+main() =>  runApp(PerguntaApp());
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0 ;
+
+  void _responder() {
+    setState(() {
+      _perguntaSelecionada++;      
+    });
+    print(_perguntaSelecionada);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Map<String, Object>> perguntas = [
+      {
+        'texto': "Qual é sua cor favorita?",
+        'respostas': ['Vermelho','Azul','Laranja', 'Preto']
+      },
+      {
+      "texto": "Qual seu animal favorito?",
+      'respostas': ['Cachorro','Gato','Cobra', 'Papagaio']
+      },
+      {
+      "texto": "Qual seu instrutor favorito?",
+      'respostas': ['João','Maria','Pedro', 'Lucas']
+      }
+    ];
+
+    List<String> respostas = perguntas[_perguntaSelecionada].cast()['respostas'];
+   // List<Widget> widgets = respostas
+   
+
+    // for(String textoResp in perguntas[_perguntaSelecionada].cast()['respostas']) { // Outra forma de percorrer
+    //   respostas.add(Resposta(textoResp, _responder));
+    // }
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text("Minha Barra de Título")), // Título da barra azul
+          backgroundColor: Colors.blue, // Opcional: Define a cor da barra (azul é o padrão)
+        ),
+        body: Center( // Centraliza o conteúdo no corpo da tela
+          child: Column(
+            children: <Widget>[
+              Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
+              ...respostas.map((t) => Resposta(t, _responder))
+            ],
+          ), // Seu texto centralizado
+        ),
+      ),
+    );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+
+  // State<PerguntaApp> createState(){
+  //   return PerguntaAppState(); 
+  // }
+
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();    
+  }
+
+
+}
